c820f3d0146e9efe48cbe2607157407cb0ab2a37
Mason Stafford <masonstafford00@gmail.com>
2020-02-09 15:56:08 -0500

    js code needed to look better.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
 
-for (let i = 0; i < 18 ;i++) {
-   var todo = localStorage.getItem("hour-"+i)
-   if(todo){
-       $("#textArea"+i).val(todo)
-   }
+for (let i = 0; i < 18; i++) {
+    var todo = localStorage.getItem("hour-" + i)
+    if (todo) {
+        $("#textArea" + i).val(todo)
+    }
 }
-$(".saveBtn").on("click", function() {
+$(".saveBtn").on("click", function () {
     var hour = $(this).val();
-   var todo = $("#textArea"+hour).val()
-   console.log(todo)
-   localStorage.setItem("hour-"+hour, todo)
+    var todo = $("#textArea" + hour).val()
+    console.log(todo)
+    localStorage.setItem("hour-" + hour, todo)
 })
 
-var displayTime = setInterval(function() {
+var displayTime = setInterval(function () {
     var currentDateAndTime = Date(Date.now());
     $("#currentDay").text(currentDateAndTime)
-},1000);
+}, 1000);
 
 var currentHour = new Date().getHours();
 $("#textArea" + currentHour).removeClass("future")
 $("#textArea" + currentHour).addClass("present")
 
 for (var j = 9; j < currentHour; j++) {
-    $("#textArea"+j).removeClass()
-    $("#textArea"+j).addClass("col-8 past")
+    $("#textArea" + j).removeClass()
+    $("#textArea" + j).addClass("col-8 past")
 }
